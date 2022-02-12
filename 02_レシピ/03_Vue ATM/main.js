adb61cca3fbe33c3494bb40a780398b82bd70e21
tomo-0119 <19s8001@ogu.ac.jp>
2022-02-12 18:54:35 +0900

    Week5-1 Vueの復習

--- 02_レシピ/03_Vue ATM/main.js
+++ 02_レシピ/03_Vue ATM/main.js
@@ -1,17 +1,58 @@
-const nyuuRyokuGaku = document.getElementById("nyuuryokugaku")
-const nyuuKin = document.getElementById("nyuukin")
-const syukKin = document.getElementById("syukkin")
-const zanDaka = document.getElementById("zandaka")
-const hello = document.getElementById("hello")
+// const nyuuRyokuGaku = document.getElementById("nyuuryokugaku")
+// const nyuuKin = document.getElementById("nyuukin")
+// const syukKin = document.getElementById("syukkin")
+// const zanDaka = document.getElementById("zandaka")
+const now = new Date()
 
-//  入力額表示方法？
-nyuuRyokuGaku.textContent = ""
+//  入力額を表示するには？
+//入力額にinputタグの入力文字を反映するには？
+Vue.createApp({
+  data() {
+    return {
+      //deposit_amountの中身
+      deposit_amount: 0,
+      //zandakaの中身
+      zandaka: 0,
+      //logの中身
+      logs: [
+        {
+          date: now,
+          type: "入金出金のどっちか",
+          money: "金額表示",
+        },
+        {
+          date: "日付",
+          type: "入金出金のどっちか",
+          money: "金額表示",
+        },
+      ],
+    }
+  },
+  methods: {
+    deposit: function () {
+      //入金の処理
+      this.zandaka += Number(this.deposit_amount)
+      // 入金後の取引記録を表示
+      this.logs.push(this.logs)
+    },
+    // 出金の処理
+    withdraw: function () {
+      this.zandaka -= Number(this.deposit_amount)
+    },
+  },
+}).mount("#deposit_amount")
 
-// 入力ボタンの処理
-nyuuKin.onclick = function () {
-  // 表示の変更方法？
-  zanDaka.textContent = zanDaka.textContent + nyuuRyokuGaku.textContent
-  hello.textContent = "こんばんは"
-}
-// 出金ボタンの処理
-syukKin.onclick = function () {}
+// Vue.createApp({
+//   data() {
+//     return {
+//       zandaka: 0,
+//     }
+//   },
+// }).mount("#zandaka")
+
+// Vue.createApp({
+//   //取引記録の追加を行う
+//   data() {
+//     return {}
+//   },
+// }).mount("#logs")
